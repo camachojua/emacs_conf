0a308f1aa283d06469cdab9de6a91c13c9226e8f
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2019-08-13 08:47:05 -0500

    Updated README

--- init.el
+++ init.el
@@ -667,6 +667,12 @@
 (use-package request :ensure t)
 
 
+;; NASM mode
+(use-package nasm-mode
+  :ensure t
+  :config
+  (add-hook 'asm-mode-hook 'nasm-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
