34fd7100411a567bbaf35bd234b7788136635cc4
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2020-05-12 10:37:23 -0500

    Correción de mu para inicializar el correo.

--- init.el
+++ init.el
@@ -910,7 +910,7 @@
 	  "--tab-width" "2"
 	  "--use-tabs" "false"
 	  "--bracket-spacing" "true"
-	  "--no-semi" "true"
+	  "--no-semi"
 	  "--single-quote" "true"
 	  "--semi" "true"
 	  "--jsx-single-quote" "true"
