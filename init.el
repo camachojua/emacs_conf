78187afa1f20826abee16dea6a789ba22ef143c0
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2020-07-13 11:06:14 -0500

    Actualizaciones para `eaf`.

--- init.el
+++ init.el
@@ -1082,25 +1082,12 @@
   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
   (eaf-bind-key take_photo "p" eaf-camera-keybinding))
 
-;; (require 'seq)
+(require 'seq)
 
-;; (setq process-environment (seq-filter
-;;  (lambda(var)
-;;    (and (not (string-match-p "QT_SCALE_FACTOR" var))
-;;         (not (string-match-p "QT_SCREEN_SCALE_FACTOR" var)))) process-environment))
+(setq process-environment (seq-filter
+ (lambda(var)
+   (and (not (string-match-p "QT_SCALE_FACTOR" var))
+        (not (string-match-p "QT_SCREEN_SCALE_FACTOR" var)))) process-environment))
 
-;; (eaf-setq eaf-browser-default-zoom  "2")
+(eaf-setq eaf-browser-default-zoom  "2")
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(yafolding winum websocket w3m vlf vdiff-magit use-package treemacs-projectile treemacs-magit treemacs-icons-dired tide request react-snippets rainbow-mode rainbow-delimiters python-pytest pug-mode projectile-rails prettier-js plantuml-mode pdf-tools org-mime ob-async nov nasm-mode multi-vterm mu4e-alert magit-todos magit-tbdiff magit-org-todos magit-filenotify magit-delta jest ivy-rich ivy-posframe htmlize highlight-indent-guides haskell-mode forge emojify doom-themes doom-modeline dockerfile-mode docker-compose-mode docker diminish diff-hl dashboard counsel-projectile company-go company-emoji clojure-mode chocolate-theme birds-of-paradise-plus-theme auctex all-the-icons-ivy all-the-icons-dired add-node-modules-path)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4")))))
