d4d0712abf3c2636e168084ec7dd64189c83fc18
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2020-07-20 12:16:27 -0500

    Añade soporte para terraform.

--- init.el
+++ init.el
@@ -1131,17 +1131,18 @@
   :defer t
   :after (circe))
 
+;;;;;;;;;;;;;;;;;;;;;;;
+;; Terraform support ;;
+;;;;;;;;;;;;;;;;;;;;;;;
+(use-package terraform-mode
+  :ensure t
+  :defer t
+  :config
+  (company-terraform-init))
+
+(use-package company-terraform
+  :ensure t
+  :defer t
+  :after terraform-mode)
+
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(circe-notifications circe camcorder magit-gitflow magit-flow yafolding winum websocket w3m vlf vdiff-magit use-package treemacs-projectile treemacs-magit treemacs-icons-dired tide request react-snippets rainbow-mode rainbow-delimiters python-pytest pug-mode projectile-rails prettier-js plantuml-mode pdf-tools org-mime ob-async nov nasm-mode multi-vterm mu4e-alert magit-todos magit-tbdiff magit-org-todos magit-filenotify magit-delta jest ivy-rich ivy-posframe htmlize highlight-indent-guides haskell-mode forge emojify doom-themes doom-modeline dockerfile-mode docker-compose-mode docker diminish diff-hl dashboard counsel-projectile company-go company-emoji clojure-mode chocolate-theme birds-of-paradise-plus-theme auctex all-the-icons-ivy all-the-icons-dired add-node-modules-path)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4")))))
