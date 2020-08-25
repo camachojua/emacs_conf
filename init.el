c678a532ee063d3a2d06e2cd85acd0945850c1c1
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2020-08-25 14:02:22 -0500

    Just a small fix.

--- init.el
+++ init.el
@@ -774,6 +774,9 @@
         smtpmail-smtp-service 587))
 
 (use-package org-mu4e
+  :ensure t
+  :defer t
+  :after mu4e
   :config
   (setq org-capture-templates
       '(("t" "todo" entry
@@ -1145,7 +1148,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(edit-indirect yafolding winum websocket w3m vlf vdiff-magit use-package treemacs-projectile treemacs-magit treemacs-icons-dired tide react-snippets rainbow-mode rainbow-delimiters python-pytest pug-mode projectile-rails prettier-js plantuml-mode pdf-tools org-mime ob-async nov nasm-mode multi-vterm mu4e-alert magit-todos magit-tbdiff magit-org-todos magit-gitflow magit-filenotify magit-delta jest ivy-rich ivy-posframe htmlize highlight-indent-guides haskell-mode forge emojify doom-themes doom-modeline dockerfile-mode docker-compose-mode docker diminish diff-hl dashboard counsel-projectile company-terraform company-go company-emoji clojure-mode circe-notifications chocolate-theme camcorder btc-ticker bitlbee birds-of-paradise-plus-theme auctex all-the-icons-ivy all-the-icons-dired add-node-modules-path)))
+   '(org-mu4e edit-indirect yafolding winum websocket w3m vlf vdiff-magit use-package treemacs-projectile treemacs-magit treemacs-icons-dired tide react-snippets rainbow-mode rainbow-delimiters python-pytest pug-mode projectile-rails prettier-js plantuml-mode pdf-tools org-mime ob-async nov nasm-mode multi-vterm mu4e-alert magit-todos magit-tbdiff magit-org-todos magit-gitflow magit-filenotify magit-delta jest ivy-rich ivy-posframe htmlize highlight-indent-guides haskell-mode forge emojify doom-themes doom-modeline dockerfile-mode docker-compose-mode docker diminish diff-hl dashboard counsel-projectile company-terraform company-go company-emoji clojure-mode circe-notifications chocolate-theme camcorder btc-ticker bitlbee birds-of-paradise-plus-theme auctex all-the-icons-ivy all-the-icons-dired add-node-modules-path)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
