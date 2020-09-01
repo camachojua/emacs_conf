8f374e759addd3b6634253a8182d754875f30b17
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2020-08-31 20:01:13 -0500

    Agrega un mejor soporte para los emojis.

--- init.el
+++ init.el
@@ -816,6 +816,11 @@
   :config
   (add-to-list 'company-backends 'company-emoji))
 
+(use-package unicode-fonts
+  :ensure t
+  :config
+  (unicode-fonts-setup))
+
 ;;;;;;;;;;;;;;;;;;;;;
 ;; Company support ;;
 ;;;;;;;;;;;;;;;;;;;;;
@@ -1141,3 +1146,16 @@
   :defer t)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(unicode-fonts yafolding winum websocket w3m vlf vdiff-magit use-package treemacs-projectile treemacs-magit treemacs-icons-dired tide react-snippets rainbow-mode rainbow-delimiters python-pytest pug-mode projectile-rails prettier-js plantuml-mode pdf-tools org-mime ob-async nov nasm-mode multi-vterm mu4e-alert magit-todos magit-tbdiff magit-org-todos magit-gitflow magit-filenotify magit-delta jest ivy-rich ivy-posframe htmlize highlight-indent-guides haskell-mode forge emojify edit-indirect doom-themes doom-modeline dockerfile-mode docker-compose-mode docker diminish diff-hl dashboard counsel-projectile company-terraform company-go company-emoji clojure-mode circe-notifications chocolate-theme camcorder btc-ticker bitlbee birds-of-paradise-plus-theme auctex all-the-icons-ivy all-the-icons-dired add-node-modules-path)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-bar ((t (:background "#6272a4")))))
