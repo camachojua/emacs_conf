1308a15723852f5f17f0c1df2bfa774db0e6854e
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2020-10-14 21:22:56 -0500

    Mantenimiento general.

--- init.el
+++ init.el
@@ -1044,3 +1044,15 @@
   :defer t)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-log-types '((lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-bar ((t (:background "#6272a4")))))
