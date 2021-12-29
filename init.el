c99e3dfa25306aa4efa216409882926d012c5b7c
Juan Camacho <juancamacho@ciencias.unam.mx>
2021-12-28 18:09:45 -0600

    (fix) Corrige error de dedo

--- init.el
+++ init.el
@@ -639,7 +639,7 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 (add-to-list 'auto-mode-alist '("Puppetfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Appraisals\\'" . ruby-mode)
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . ruby-mode)))
 
 (use-package projectile-rails
   :straight t
