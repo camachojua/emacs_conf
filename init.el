3c76d8fb2372464e9721ddd33a0ebd17588ea7f6
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2023-06-02 19:39:38 -0600

    feat: Usa ts-mode en varios modos de lenguajes de programación

--- init.el
+++ init.el
@@ -1148,6 +1148,16 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 
 (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
 
+(setq major-mode-remap-alist
+ '((yaml-mode . yaml-ts-mode)
+   (bash-mode . bash-ts-mode)
+   (js2-mode . js-ts-mode)
+   (typescript-mode . typescript-ts-mode)
+   (json-mode . json-ts-mode)
+   (css-mode . css-ts-mode)
+   (python-mode . python-ts-mode)
+   (ruby-mode . ruby-ts-mode)))
+
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; Ruby development ;;
 ;;;;;;;;;;;;;;;;;;;;;;
