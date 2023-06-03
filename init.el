3b9f7df67399ab703cb530e6b13a0f6266dba31b
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2023-06-02 19:38:38 -0600

    feat: Agrega gramáticas de tree-sitter

--- init.el
+++ init.el
@@ -1127,17 +1127,26 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 ;;;;;;;;;;;;;;;;;
 ;; Tree Sitter ;;
 ;;;;;;;;;;;;;;;;;
-(use-package tree-sitter
-  :straight t
-  :init
-  (require 'tree-sitter)
-  (global-tree-sitter-mode))
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (haskell "https://github.com/tree-sitter/haskell-tree-sitter" "master" "tree-sitter-haskell")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
-(use-package tree-sitter-langs
-  :straight t
-  :after tree-sitter-mode
-  :init
-  (require 'tree-sitter-langs))
+(mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; Ruby development ;;
