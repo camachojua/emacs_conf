7c2f72d95f0a8816ba47d3bfa21d986e141896a4
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2023-06-10 09:32:17 -0600

    feat: Agrega projectile-rails

--- init.el
+++ init.el
@@ -651,4 +651,38 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   (robe-start)
   :hook
   (ruby-ts-mode . robe-mode))
+
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.cap\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Thorfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Podfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.podspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Puppetfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Appraisals\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+
+(use-package projectile-rails
+  :straight t
+  :hook
+  (after-init . projectile-rails-global-mode)
+  (after-init . inf-ruby-switch-setup)
+  :config
+  (setq projectile-rails-spring-command "~/Src/buk-webapp/bin/spring"
+	projectile-rails-vanilla-command "~/Src/buk-webapp/bin/rails")
+  :bind-keymap
+  ("C-c r" . projectile-rails-command-map))
 ;;; init.el ends here
