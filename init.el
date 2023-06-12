e152b1d18e7e4b7e92917d27b4a3038dddf16955
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2023-06-12 09:42:41 -0600

    feat: Mejoras a eshell

--- init.el
+++ init.el
@@ -1009,4 +1009,75 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   (setq olivetti-body-width 80)
   :hook
   (text-mode . olivetti-mode))
+
+;;;;;;;;;;;;
+;; Eshell ;;
+;;;;;;;;;;;;
+(use-package eshell-prompt-extras
+  :straight t
+  :config
+  (autoload 'epe-theme-lambda "eshell-prompt-extras")
+  (setq eshell-highlight-prompt nil
+	eshell-prompt-function 'epe-theme-lambda))
+
+(require 'em-term)
+(add-to-list 'eshell-visual-commands "htop nano vim")
+(setq eshell-destroy-buffer-when-process-dies t)
+
+(defun eshell-exec-visual (&rest args)
+   "Run the specified PROGRAM in a terminal emulation buffer.
+ ARGS are passed to the program.  At the moment, no piping of input is
+ allowed."
+   (let* (eshell-interpreter-alist
+	   (original-args args)
+	   (interp (eshell-find-interpreter (car args) (cdr args)))
+	   (in-ssh-tramp (and (tramp-tramp-file-p default-directory)
+			      (equal (tramp-file-name-method
+				      (tramp-dissect-file-name default-directory))
+				     "ssh")))
+	   (program (if in-ssh-tramp
+			"ssh"
+		      (car interp)))
+	   (args (if in-ssh-tramp
+		     (let ((dir-name (tramp-dissect-file-name default-directory)))
+		       (eshell-flatten-list
+			(list
+			 "-t"
+			 (tramp-file-name-host dir-name)
+			 (format
+			  "export TERM=xterm-256color; cd %s; exec %s"
+			  (tramp-file-name-localname dir-name)
+			  (string-join
+			   (append
+			    (list (tramp-file-name-localname (tramp-dissect-file-name (car interp))))
+			    (cdr args))
+			   " ")))))
+		   (eshell-flatten-list
+		    (eshell-stringify-list (append (cdr interp)
+						   (cdr args))))))
+	   (term-buf
+	    (generate-new-buffer
+	     (concat "*"
+		     (if in-ssh-tramp
+			 (format "%s %s" default-directory (string-join original-args " "))
+			 (file-name-nondirectory program))
+		     "*")))
+	   (eshell-buf (current-buffer)))
+     (save-current-buffer
+	(switch-to-buffer term-buf)
+	(term-mode)
+	(set (make-local-variable 'term-term-name) eshell-term-name)
+	(make-local-variable 'eshell-parent-buffer)
+	(setq eshell-parent-buffer eshell-buf)
+	(term-exec term-buf program program nil args)
+	(let ((proc (get-buffer-process term-buf)))
+	  (if (and proc (eq 'run (process-status proc)))
+	      (set-process-sentinel proc 'eshell-term-sentinel)
+	    (error "Failed to invoke visual command")))
+	(term-char-mode)
+	(if eshell-escape-control-x
+	    (term-set-escape-char ?\C-x))))
+   nil)
+
+(setq eshell-history-size 1000000)
 ;;; init.el ends here
