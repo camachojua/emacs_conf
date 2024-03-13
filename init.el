92d32b3ba39f77fcdb0c3b5aa3c5b66b0e42b16c
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2024-03-13 14:23:53 -0600

    feat: Vuelve a ajustar la transparencia del frame principal

--- init.el
+++ init.el
@@ -83,8 +83,8 @@
 (setq global-auto-revert-non-file-buffers t)
 
 ;; Transparent background
-(set-frame-parameter nil 'alpha-background 90)
-(add-to-list 'default-frame-alist '(alpha-background . 90))
+(set-frame-parameter nil 'alpha-background 95)
+(add-to-list 'default-frame-alist '(alpha-background . 95))
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; TRAMP settings ;;
