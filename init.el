9a9aa252388bbfbdad499a82fdad4db601796dbd
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2024-05-16 18:40:05 -0600

    feat: retrasa la ejecución de robe

--- init.el
+++ init.el
@@ -774,6 +774,7 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 (use-package robe
   :straight t
   :defer t
+  :after ruby-mode
   :init
   (setq inf-ruby-console-rails buk-webapp-dir)
   :config
