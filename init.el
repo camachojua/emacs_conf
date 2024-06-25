6952a912acfeaac3e34e47908cef5b7f37984ccc
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2024-06-25 09:15:59 -0600

    fix: Elimina dependencia de inf-ruby

--- init.el
+++ init.el
@@ -814,6 +814,7 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   (ruby-ts-mode . rubocop-mode))
 
 (use-package inf-ruby
+  :defer t
   :straight t
   :after ruby-mode
   :hook
@@ -822,7 +823,6 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   :config
   (setq show-trailing-whitespace nil)
   (setq inf-ruby-default-implementation "pry")
-  (linum-mode 0)
   (inf-ruby-enable-auto-breakpoint))
 
 (use-package minitest
